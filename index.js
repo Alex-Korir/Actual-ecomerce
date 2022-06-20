7a94775b536821e99c55c72e8bf31aedfc6b24dc
Alex-Korir <alexkogo63@gmail.com>
2022-06-20 23:15:53 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 const btn_images = document.querySelectorAll(".small-images");
 const dis_img = document.querySelector("#display-image");
+const thumbnails = document.querySelectorAll("#thumbnails");
 
-// /*MAKING ONE BUTTON BECOME ACTIVE AT A TIME*/
-// const selection = document.querySelector("#select");
-// //selecting all the buttons and giving them an event
-// btn_images.forEach((div) => {
-//   div.addEventListener("click", function () {
-//     // Bubbling inside the whole div
-//     selection.querySelector(".active").classList.remove("active");
-//     div.classList.add("active");
-//   });
-// });
+/*MAKING ONE BUTTON BECOME ACTIVE AT A TIME*/
+
+const selection = document.querySelector("#select");
+//selecting all the buttons and giving them an event
+btn_images.forEach((div) => {
+  div.addEventListener("click", function () {
+    // Bubbling inside the whole div
+    selection.querySelector(".active").classList.remove("active");
+    div.classList.add("active");
+  });
+});
 
 /*SELECT ONE BUTTON AND MAKE IT CHANGE THE LARGER ELEMENT*/
 
-for (i = 0; i <= btn_images.length; i++) {
-  //console.log(btn_images[i]);
-  btn_images[i].addEventListener("click", runAway);
+for (i = 0; i < thumbnails.length; i++) {
+  thumbnails[i].addEventListener("click", runAway);
   function runAway() {
-    console.log("Are you crazy");
-    house = this.value;
-    if ((house = 1)) {
-      dis_img.innerHTML =
-        "url(/ecommerce-product-page-main/images/image-product-2.jpg)";
+    const house = this.dataset.value;
+    const image_one =
+      "url('/ecommerce-product-page-main/images/image-product-1.jpg')";
+    const image_two =
+      "url('/ecommerce-product-page-main/images/image-product-2.jpg')";
+    const image_three =
+      "url('/ecommerce-product-page-main/images/image-product-3.jpg')";
+    const image_four =
+      "url('/ecommerce-product-page-main/images/image-product-4.jpg')";
+    if (house === "1") {
+      console.log("looming");
+      dis_img.style.backgroundImage = image_one;
+    } else if (house === "2") {
+      dis_img.style.backgroundImage = image_two;
+    } else if (house === "3") {
+      dis_img.style.backgroundImage = image_three;
+    } else if (house === "4") {
+      dis_img.style.backgroundImage = image_four;
+    } else {
+      dis_img.style.backgroundImage = image_one;
     }
   }
 }
